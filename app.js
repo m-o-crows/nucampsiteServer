30e47b15c208fe36bc03d2f2c7a6367ca3f8886b
Omi Masika <omimasika@gmail.com>
2022-02-12 09:01:20 -0500

    CORS

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
+const mongoose = require("mongoose");
 const passport = require("passport");
-const authenticate = require("./authenticate");
 const config = require("./config");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-const campsiteRouter = require("./routes/campsiteRouter");
-const promotionRouter = require("./routes/promotionRouter");
-const partnerRouter = require("./routes/partnerRouter");
-
-const mongoose = require("mongoose");
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
@@ -25,19 +15,24 @@ const connect = mongoose.connect(url, {
 });
 
 connect.then(
-  () => console.log("Connected correctly to the server"),
+  () => console.log("Connected correctly to server"),
   (err) => console.log(err)
 );
 
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require("./routes/uploadRouter");
 
-// Secure traffic only
+var app = express();
 app.all("*", (req, res, next) => {
-  if (req.secure) {
+  if (!!req.secure) {
     return next();
   } else {
     console.log(
-      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+      `Redirecting to https://${req.hostname}:${app.get("secPort")}${req.url}`
     );
     res.redirect(
       301,
@@ -53,42 +48,19 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser('12345-67890-09876-54321'));
-
-app.use(
-  session({
-    name: "session-id",
-    secret: "12345-67890-09876-54321",
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore(),
-  })
-);
+// app.use(cookieParser("12345-67890-09876-54321"));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    const err = new Error("You are not authenticated!");
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  }
-}
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campsites", campsiteRouter);
-app.use("/partners", partnerRouter);
 app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
